b20a229d71faea7d10a7a01ea784457d8ee30a61
GoranStambukZadar <goran.stambuk.zadar@gmail.com>
2025-08-31 03:13:04 -0700

    Add files via upload

--- Bin/GSecurity.cmd
+++ Bin/GSecurity.cmd
@@ -128,24 +128,9 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "Con
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorUser" /t REG_DWORD /d "1" /f
 
 :: Perms
-icacls "%systemdrive%\Users" /remove "Everyone"
-takeown /f "%USERPROFILE%\Desktop" /r /d y
-icacls "%USERPROFILE%\Desktop" /inheritance:r
-icacls "%USERPROFILE%\Desktop" /grant:r %username%:(OI)(CI)F /t /l /q /c
-icacls "%USERPROFILE%\Desktop" /remove "System" /t /c /l
-icacls "%USERPROFILE%\Desktop" /remove "Administrators" /t /c /l
-icacls "C:\Users\Public" /reset /T
-takeown /f "C:\Users\Public\Desktop" /r /d y
-icacls "C:\Users\Public\Desktop" /inheritance:r
-icacls "C:\Users\Public\Desktop" /grant:r %username%:(OI)(CI)F /t /l /q /c
-icacls "C:\Users\Public\Desktop" /remove "System" /t /c /l
-icacls "C:\Users\Public\Desktop" /remove "Administrators" /t /c /l
 takeown /f %windir%\System32\Oobe\useroobe.dll /A
 icacls %windir%\System32\Oobe\useroobe.dll /inheritance:r
 for %%D in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
         icacls "%%D:\" /inheritance:d /remove:g "Authenticated Users" /C
         icacls "%%D:\" /grant:r "Authenticated Users:(RX)" /C
 )
-
-:: Security Policy
-LGPO.exe /s GSecurity.inf
