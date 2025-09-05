d356370fc3d3d7e050f1d8225afa00cf0d78c7e7
GoranStambukZadar <goran.stambuk.zadar@gmail.com>
2025-09-05 16:53:11 +0200

    Update SetupComplete.cmd

--- Iso/sources/$OEM$/$$/Setup/Scripts/SetupComplete.cmd
+++ Iso/sources/$OEM$/$$/Setup/Scripts/SetupComplete.cmd
@@ -1,9 +1,1 @@
-@echo off
->nul 2>&1 fsutil dirty query %systemdrive% || echo CreateObject^("Shell.Application"^).ShellExecute "%~0", "ELEVATED", "", "runas", 1 > "%temp%\uac.vbs" && "%temp%\uac.vbs" && exit /b
-DEL /F /Q "%temp%\uac.vbs"
-setlocal EnableExtensions EnableDelayedExpansion
-cd /d %~dp0
-cd Bin
-for /f "tokens=*" %%A in ('dir /b /o:n *.cmd') do (
-     call "%%A"
-)
+
